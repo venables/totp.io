639ecef930a945100b926581ae81aa143b25aea5
Matt Venables <mattvenables@gmail.com>
2015-03-03 20:35:36 -0500

    Add a homepage, and logging

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 'use strict';
 
+try {
+  require('dotenv').load();
+} catch(e) {}
 var express = require('express');
+
 var app = express();
+var morgan = require('morgan')
 var nodalytics = require('nodalytics');
 var port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev')); // combined
 app.use(nodalytics('UA-60297301-1'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/', function(req, res) {
+  res.render('pages/home');
 })
 
 app.get('/:token', function(req, res) {
-  res.render('token', { token: req.params.token });
+  res.render('pages/token', { token: req.params.token });
 });
 
 var server = app.listen(port, function () {
   var host = server.address().address
   var port = server.address().port
+
   console.log('Example app listening at http://%s:%s', host, port)
 });
